92f4cae1c4bb2195e324734147fdd0516b1d90d0
davimacana <macana.davi@gmail.com>
2018-11-27 22:51:13 -0200

    Commit Spring MVC Aula 2.

--- Projeto2/src/main/java/br/com/spring/controller/InicioController.java
+++ Projeto2/src/main/java/br/com/spring/controller/InicioController.java
@@ -3,17 +3,20 @@ package br.com.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Davi Maçana
  *
  */
 @Controller
+@RequestMapping(value = "/")
 public class InicioController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String inicio() {
-		return "inicio";
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView inicio() {
+		ModelAndView model = new ModelAndView("login");
+		return model;
 	}
 
 }
